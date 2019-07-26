858b9dcd62a361ac36297fc444e8ae8951f7ac88
dirkcallaway <35826733+dirkcallaway@users.noreply.github.com>
2019-07-26 14:04:03 -0600

    Set up public folder and server for deployment

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public'))
+
+    app.get(/.*/, (req, res) => res.sendfile(__dirname + '/public/index.html'));
+}
+
 
 const PORT = process.env.PORT || 3000;
 
